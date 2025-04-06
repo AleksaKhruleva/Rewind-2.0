2845f90164127807e17a9b36f5dd3a270dc337fd
Danila Rakhmanov <71427624+flowykk@users.noreply.github.com>
2025-04-06 18:57:12 +0300

    REWIND-26: Implemented MediaDetailsView

--- Rewind/Modules/UIComponents/Sources/Modifiers/AppIconTaskModifier.swift
+++ Rewind/Modules/UIComponents/Sources/Modifiers/AppIconTaskModifier.swift
@@ -16,3 +16,37 @@ public struct AppIconTaskModifier: ViewModifier {
             )
     }
 }
+
+public struct TagViewModifier: ViewModifier {
+    private var tags: [String]
+    private var tag: String
+    private var geometry: GeometryProxy
+    
+    public func body(content: Content) -> some View {
+        var width = CGFloat.zero
+        var height = CGFloat.zero
+        
+        content
+            .alignmentGuide(.leading, computeValue: { d in
+                if (abs(width - d.width) > geometry.size.width)
+                {
+                    width = 0
+                    height -= d.height
+                }
+                let result = width
+                if tag == self.tags.last! {
+                    width = 0 //last item
+                } else {
+                    width -= d.width
+                }
+                return result
+            })
+            .alignmentGuide(.top, computeValue: {d in
+                let result = height
+                if tag == self.tags.last! {
+                    height = 0 // last item
+                }
+                return result
+            })
+    }
+}
