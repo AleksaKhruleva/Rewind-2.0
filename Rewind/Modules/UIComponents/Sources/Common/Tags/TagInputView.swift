465c86e47246708649295d15e9b12033468bd324
Danila Rakhmanov <71427624+flowykk@users.noreply.github.com>
2025-04-07 12:25:10 +0300

    REWIND-26: Implemented MediaDetailsView
    Implemented MediaDetailsView

---
+++ Rewind/Modules/UIComponents/Sources/Common/Tags/TagInputView.swift
@@ -0,0 +1,39 @@
+import SwiftUI
+
+public struct TagInputView: View {
+    private var onSubmit: (String) -> Void
+    
+    @State private var tag: String = ""
+    @FocusState private var isFocused: Bool
+    
+    @Environment(\.dismiss)
+    private var dismiss
+    
+    public init(onSubmit: @escaping (String) -> Void) {
+        self.onSubmit = onSubmit
+    }
+    
+    public var body: some View {
+        ZStack(alignment: .topLeading) {
+            VStack(alignment: .center, spacing: AuthConstants.fieldSpacing) {
+                Text("Enter new tag")
+                    .modifier(RoundFontModifier(size: AuthConstants.titleFontSize))
+                
+                StyledTextField(
+                    text: $tag,
+                    placeholder: "tag"
+                )
+                .multilineTextAlignment(.center)
+                .focused($isFocused)
+                .onSubmit {
+                    onSubmit(tag)
+                    dismiss()
+                }
+            }
+            .modifier(VStackPositionModifier(topOffsetRatio: AuthConstants.contentTopOffsetRatio))
+        }
+        .onAppear {
+            isFocused = true
+        }
+    }
+}
