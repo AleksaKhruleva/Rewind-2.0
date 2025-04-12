ed8e7cda1573511ace9a8a2d80d12e73ad48b157
Danila Rakhmanov <71427624+flowykk@users.noreply.github.com>
2025-04-12 11:25:13 +0300

    REWIND-29: Implemented ToastView

---
+++ Rewind/Modules/UIComponents/Sources/Toasts/Toast.swift
@@ -0,0 +1,53 @@
+import SwiftUI
+
+public struct ToastView: View {
+    @State
+    private var controller: ToastController
+    private var title: String
+    
+    public init(controller: ToastController) {
+        self.controller = controller
+        title = controller.state.title
+    }
+    
+    public var body: some View {
+        VStack(spacing: 0) {
+            Spacer()
+            
+            if controller.state.isPresented {
+                content
+                    .padding(.horizontal, 8)
+                    .transition(.move(edge: .bottom).combined(with: .opacity)
+                    )
+                    .onTapGesture {
+                        controller.hide()
+                    }
+            }
+        }
+    }
+    
+    private var content: some View {
+        HStack(alignment: .center, spacing: 8) {
+            Text(title)
+                .modifier(RoundFontModifier(size: 14, weight: .black, foregroundColor: .white))
+                .padding(.vertical)
+            
+            Spacer()
+        }
+        .padding(.leading)
+        .padding(.trailing)
+        .background(.black)
+        .cornerRadius(16)
+    }
+}
+
+#Preview {
+    ToastView(
+        controller: .init(
+            state: .init(
+                isPresented: true,
+                title: "Пост сохранен в избранное"
+            )
+        )
+    )
+}
