d49866969937c0e9186a0d55c8f4a2be81a24dcf
Danila Rakhmanov <71427624+flowykk@users.noreply.github.com>
2025-05-02 14:05:54 +0300

    REWIND-53: Cutted out BackButton

--- Rewind/Modules/UIComponents/Sources/Constants/AuthConstants.swift
+++ Rewind/Modules/UIComponents/Sources/Constants/AuthConstants.swift
@@ -4,7 +4,4 @@ public enum AuthConstants {
     public static let fieldSpacing: CGFloat = 21
     public static let titleFontSize: CGFloat = 15
     public static let contentTopOffsetRatio: CGFloat = 0.4
-    
-    public static let backButtonXRatio: CGFloat = 42 / 393
-    public static let backButtonYRatio: CGFloat = 35 / 852
 }
