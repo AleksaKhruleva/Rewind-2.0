4930d574ee064397a47310cc4290dff826bf9015
Aleksa Khruleva <66669044+AleksaKhruleva@users.noreply.github.com>
2025-05-12 10:11:03 +0300

    REWIND-90: Implemented ability to get userID from access token

---
+++ Rewind/Modules/Base/Sources/JWTDecoderService.swift
@@ -0,0 +1,30 @@
+import Foundation
+
+public struct JWTDecoderService {
+    
+    public init() {}
+    
+    public func decode(jwtToken: String) -> [String: Any]? {
+        let segments = jwtToken.components(separatedBy: ".")
+        guard segments.count > 1 else { return nil }
+        
+        var base64String = segments[1]
+        let remainder = base64String.count % 4
+        if remainder > 0 {
+            base64String += String(repeating: "=", count: 4 - remainder)
+        }
+        
+        guard let data = Data(base64Encoded: base64String),
+              let json = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] else {
+            return nil
+        }
+        
+        return json
+    }
+    
+    public func getUserId(from jwtToken: String) -> Int? {
+        guard let payload = decode(jwtToken: jwtToken) else { return nil }
+        
+        return payload["user_id"] as? Int
+    }
+}
