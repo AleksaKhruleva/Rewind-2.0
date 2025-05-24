718859b191f77c36b8e4a4873620f97b3fab6a89
Aleksandra Khruleva <adkhruleva@edu.hse.ru>
2025-05-24 15:19:43 +0300

    Made some changes after the appearance of the view model

--- Rewind/Modules/Domain/Sources/MediaItem.swift
+++ Rewind/Modules/Domain/Sources/MediaItem.swift
@@ -7,20 +7,17 @@ public enum MediaType {
 }
 
 public struct MediaItem {
-    public let id: UUID
     public let type: MediaType
     public let image: UIImage?
     public let track: Track?
     public let videoURL: URL?
     
     public init(
-        id: UUID = UUID(),
         type: MediaType,
         image: UIImage? = nil,
         track: Track? = nil,
         videoURL: URL? = nil
     ) {
-        self.id = id
         self.type = type
         self.image = image
         self.track = track
--- Rewind/Modules/Features/Sources/Rewind/RewindView.swift
+++ Rewind/Modules/Features/Sources/Rewind/RewindView.swift
@@ -7,13 +7,9 @@ public struct RewindView: View {
     @State private var viewModel: RewindViewModel
     
     // TODO: To VM
-    @State private var rolls = 0
-    @State private var currentIndex = 0
     @State private var filterSettingsShown = false
     @State private var isSelectGroupPresented = false
-    @State private var isTrackPlaying = false
     
-    private var mediaItems = [MediaItem]()
     private let router: RewindRouter
     
     @Environment(\.showToast)
@@ -22,52 +18,6 @@ public struct RewindView: View {
     public init(router: RewindRouter) {
         self.router = router
         viewModel = RewindViewModel()
-        
-        let json = """
-        {
-          "id": 2026794888,
-          "title": "миражи — кружок хора (>∆<)",
-          "artwork_url": "https://i1.sndcdn.com/artworks-1WyHHVfSQvKviNzI-bP1zeA-large.jpg",
-          "duration": 247063,
-          "media": {
-            "transcodings": [
-              {
-                "url": "https://api-v2.soundcloud.com/media/soundcloud:tracks:2026794888/cf1a0f7f-7d0e-4ce8-b601-656593f23ab3/stream/progressive",
-                "preset": "mp3_1_0",
-                "duration": 247066,
-                "snipped": false,
-                "format": {
-                  "protocol": "progressive",
-                  "mime_type": "audio/mpeg"
-                },
-                "quality": "sq",
-                "is_legacy_transcoding": true
-              }
-            ]
-          },
-          "user": {
-            "username": "skibidi rizz"
-          }
-        }
-        """
-        
-        let data = Data(json.utf8)
-        var track = try? JSONDecoder().decode(Track.self, from: data)
-        track?.streamURL = URL(
-            string: "https://cf-media.sndcdn.com/wHq5ExUUltpj.128.mp3?Policy=eyJTdGF0ZW1lbnQiOlt7IlJlc291cmNlIjoiKjovL2NmLW1lZGlhLnNuZGNkbi5jb20vd0hxNUV4VVVsdHBqLjEyOC5tcDMqIiwiQ29uZGl0aW9uIjp7IkRhdGVMZXNzVGhhbiI6eyJBV1M6RXBvY2hUaW1lIjoxNzQ4MDg2NzM5fX19XX0_&Signature=AHmH2FSsRjQsX5IxrWdvs3VJz9hQ06N8T~olBA5KuC-bxU2z1ziP2Fe1eyEQoLNm3uauVQweHCegLlE95sSyyJ0J-cnuEsMUrzo4KZFIFk~e7TykIEToaA1QVsihYD4he5tMrvDksRAAWYvYMt-tA21IsObQ84UDzD--vq~SgNhtuzpH-f9GM7R5bsuvKzOiTP6f0eDkCD9SpzhPTrQ2NJb1ZcGtkDEfnckStMQd15TNP1~hFDO4dFgMMyLzhVOf43bl1U7x4nKlqyrbpjIGHLFB62x5OIGmpa7YvYjX42vcBaPml-7eH6qEZWcUrDQnRpaEo~MOM9iYw0bz4wa7Pg__&Key-Pair-Id=APKAI6TU7MMXM5DG6EPQ"
-        )
-        
-        mediaItems = [
-            MediaItem(
-                type: .imageWithMusic,
-                image: UIComponentsAsset.media21.image,
-                track: track
-            ),
-            MediaItem(
-                type: .image,
-                image: UIComponentsAsset.media16.image
-            )
-        ]
     }
     
     public var body: some View {
@@ -86,7 +36,7 @@ public struct RewindView: View {
                 HStack {
                     author
                     Spacer()
-                    RewindRollsStat(rolls: $rolls)
+                    RewindRollsStat(rolls: $viewModel.rolls)
                 }
             }
             .modifier(VStackTopOffsetModifier(topOffsetRatio: 0.15))
@@ -117,7 +67,7 @@ public struct RewindView: View {
         }
         .onAppear {
             viewModel.set(showToast: showToast)
-            Task { await viewModel.dispatch(.fetchUser) }
+            viewModel.dispatch(.fetchUser)
         }
         .ignoresSafeArea(.keyboard)
         .sheet(isPresented: $isSelectGroupPresented) {
@@ -159,31 +109,16 @@ public struct RewindView: View {
     
     private var mediaView: some View {
         MediaContentView(
-            mediaItem: mediaItems[currentIndex],
+            mediaItem: viewModel.currentMediaItem,
             onSave: {},
             onLike: {},
             onToggleSound: {
-                if !isTrackPlaying {
-                    if let url = mediaItems[currentIndex].track?.streamURL {
-                        AudioPlayerManager.shared.load(url: url)
-                        AudioPlayerManager.shared.play(from: .zero) { isPlaying in
-                            isTrackPlaying = true
-                        }
-                    }
-                } else {
-                    isTrackPlaying = false
-                    AudioPlayerManager.shared.stop()
-                }
+                viewModel.dispatch(.toggleTrackPlaying)
             },
-            isTrackPlaying: $isTrackPlaying
+            isTrackPlaying: $viewModel.isTrackPlaying
         )
         .onTapGesture {
-            withAnimation {
-                isTrackPlaying = false
-                AudioPlayerManager.shared.stop()
-                rolls += 1
-                currentIndex = (currentIndex + 1) % mediaItems.count
-            }
+            viewModel.dispatch(.showNextMediaItem)
         }
     }
     
@@ -193,7 +128,7 @@ public struct RewindView: View {
                 image: UIComponentsAsset.sasha.image,
                 name: "sasha",
                 date: "22.04.2025",
-                track: mediaItems[currentIndex].track
+                track: viewModel.currentMediaItem.track
             )
         }
     }
