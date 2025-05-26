cbf4476879d98a4bc58138c8477c300eede877e2
dvmukhin <dvmukhin@avito.ru>
2025-05-26 15:14:18 +0300

    REWIND-119: fixed swagger

--- Backend/api-gateway-service/docs/docs.go
+++ Backend/api-gateway-service/docs/docs.go
@@ -3201,7 +3201,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:8080",
+	Host:             "rewindapp.ru",
 	BasePath:         "",
 	Schemes:          []string{},
 	Title:            "Rewind API Gateway",
